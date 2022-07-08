00185ee88560a1a472556e3db5cddbb9b460af88
Tihonov Egor <etihonov811@gmail.com>
2022-07-08 11:35:54 +0300

    new commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module awesomeProject
+
+go 1.18
+
+require (
+	github.com/jackc/pgx/v4 v4.16.1 // indirect
+	github.com/labstack/echo/v4 v4.7.2 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strconv"
+)
+
+type person struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Works bool   `json:"works"`
+}
+
+var (
+	users = map[int]*person{}
+	seq   = 1
+)
+
+func createUser(c echo.Context) error {
+	u := &person{
+		ID: seq,
+	}
+	err := c.Bind(u)
+	if err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func updateUser(c echo.Context) error {
+	u := new(person)
+	err := c.Bind(u)
+	if err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+func getAllUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, users)
+}
+
+func main() {
+	e := echo.New()
+
+	// Routes
+	e.GET("/users", getAllUsers)
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+	// Start server
+	e.Start(":8000")
+}
