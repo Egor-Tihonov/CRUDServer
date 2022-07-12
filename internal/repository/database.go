c64c6dc8ae9eff8f6e80a7244bcf9559c8a0cbf3
Егор Тихонов <etihonov811@gmail.com>
2022-07-12 16:31:37 +0300

    new commit 12.07.2022

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -4,7 +4,6 @@ import (
 	"awesomeProject/internal/model"
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -23,31 +22,29 @@ func (r *Repository) Create(ctx context.Context, person *model.Person) error {
 	person.Works = true
 	_, err := r.pool.Exec(ctx, "insert into persons(name,works) values($1,$2)", person.Name, person.Works)
 	if err != nil {
-		fmt.Println(err)
+		_ = fmt.Errorf("error: %v", err)
 	}
 	return err
 }
 
-func (r *Repository) SelectAll() []model.Person {
-	var persons []model.Person
+func (r *Repository) SelectAll() []*model.Person {
+	var persons []*model.Person
 
-	rows, err := r.pool.Query(context.Background(), "select * from persons")
+	rows, err := r.pool.Query(context.Background(), "select id,name,works from persons")
 	if err != nil {
-		log.Fatal(err)
+		_ = fmt.Errorf("error: %v", err)
 	}
 
 	for rows.Next() {
 		p := model.Person{}
 		err := rows.Scan(&p.ID, &p.Name, &p.Works)
+		defer rows.Close()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		persons = append(persons, p)
+		persons = append(persons, &p)
 	}
-	/*for _, p := range Persons {
-		fmt.Println(p.ID, p.Name, p.Works)
-	}*/
 	return persons
 }
 
@@ -61,24 +58,20 @@ func (r *Repository) Update(id int) error {
 	_, err := r.pool.Exec(context.Background(), "update persons set name=$1 where id=$2", name, id)
 	return err
 }
-func (r *Repository) SelectById(id int) []model.Person {
-	var persons []model.Person
-
-	rows, err := r.pool.Query(context.Background(), "select * from persons where id=$1", id)
+func (r *Repository) SelectById(id int) []*model.Person {
+	var persons []*model.Person
+	var name string
+	var works bool
+	err := r.pool.QueryRow(context.Background(), "select id,name,works from persons where id=$1", id).Scan(&id, &name, &works)
 	if err != nil {
 		return nil
 	}
-	for rows.Next() {
-		p := model.Person{}
-		err := rows.Scan(&p.ID, &p.Name, &p.Works)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		persons = append(persons, p)
+	p := model.Person{
+		ID:    id,
+		Name:  name,
+		Works: works,
 	}
-	/*for _, p := range Persons {
-		fmt.Println(p.ID, p.Name, p.Works)
-	}*/
+	persons = append(persons, &p)
+
 	return persons
 }
