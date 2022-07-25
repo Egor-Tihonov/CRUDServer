1b4b339c041c52f0bffff7692ad81257671d0d94
Tihonov Egor <etihonov811@gmail.com>
2022-07-25 15:04:28 +0300

    new commit 25.07.2022 add upload & download file add validation

--- internal/service/service.go
+++ internal/service/service.go
@@ -4,6 +4,7 @@ import (
 	"awesomeProject/internal/model"
 	"awesomeProject/internal/repository"
 	"context"
+	"fmt"
 )
 
 var JwtKey = []byte("super-key") //key fo generation and check tokens
@@ -17,14 +18,34 @@ func NewService(NewRps repository.Repository) *Service { //create
 }
 
 func (s *Service) UpdateUser(ctx context.Context, id string, person *model.Person) error { //update user
+	err := ValidateValueID(id)
+	if err != nil {
+		return fmt.Errorf("service: error to update user, validation failed")
+	}
 	return s.rps.Update(ctx, id, person)
 }
 func (s *Service) SelectAllUsers(ctx context.Context) ([]*model.Person, error) { //get all users from DB without passwords and tokens
 	return s.rps.SelectAll(ctx)
 }
 func (s *Service) DeleteUser(ctx context.Context, id string) error { //delete user from DB
+	err := ValidateValueID(id)
+	if err != nil {
+		return fmt.Errorf("service: error to delete user, validation failed")
+	}
 	return s.rps.Delete(ctx, id)
 }
 func (s *Service) GetUserById(ctx context.Context, id string) (model.Person, error) { //get one user by id
+	err := ValidateValueID(id)
+	if err != nil {
+		return model.Person{}, fmt.Errorf("service: error to get user by id, validation failed")
+	}
 	return s.rps.SelectById(ctx, id)
+}
+
+func ValidateValueID(id string) error {
+	err := validate.Var(id, "required, min=36")
+	if err != nil {
+		return fmt.Errorf("id length couldnt be less then 36,~%v", err)
+	}
+	return nil
 }
