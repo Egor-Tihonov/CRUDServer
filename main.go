f02118d3308a355ddffc4da77cbdccf19dec0c11
Егор Тихонов <etihonov811@gmail.com>
2022-08-09 10:05:39 +0300

    new commit

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"awesomeProject/internal/repository"
 	"awesomeProject/internal/service"
 	"context"
+
 	"github.com/caarlos0/env/v6"
 	"github.com/go-redis/redis/v9"
 	"github.com/jackc/pgx/v4/pgxpool"
